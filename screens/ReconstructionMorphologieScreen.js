7da4be94d86bbc1dca792c0e507a26c019a16f90
errami.fatima-ezzahrae <fatimaezzahrae.errami@hotmail.com>
2023-10-25 12:14:18 +0200

    add component buttoncamera

--- screens/ReconstructionMorphologieScreen.js
+++ screens/ReconstructionMorphologieScreen.js
@@ -1,10 +1,17 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
+import CameraButton from '../components/CameraButton'; // Import the CameraButton component
 
 const ReconstructionMorphologieScreen = () => {
+  const onPhotoTaken = (photo) => {
+    // Handle the captured photo here, e.g., save it, display it, etc.
+  };
+
   return (
     <View style={styles.container}>
       <Text>Reconstruction Morphologie Screen</Text>
+
+      <CameraButton onPhotoTaken={onPhotoTaken} /> 
     </View>
   );
 }
@@ -14,7 +21,7 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+  },
 });
 
 export default ReconstructionMorphologieScreen;
