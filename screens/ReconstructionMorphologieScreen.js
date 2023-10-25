90caf6a7c7943b95d903bd552b9a6f6109f1e4d7
jouaiti.chems <chemsjouaiti06@gmail.com>
2023-10-25 11:27:56 +0200

    add Instructions

--- screens/ReconstructionMorphologieScreen.js
+++ screens/ReconstructionMorphologieScreen.js
@@ -1,10 +1,38 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React , { useState }from 'react';
+import { View, Text, StyleSheet, Modal,Alert,Pressable } from 'react-native';
 
 const ReconstructionMorphologieScreen = () => {
+  const [modalVisible, setModalVisible] = useState(false);
   return (
     <View style={styles.container}>
-      <Text>Reconstruction Morphologie Screen</Text>
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={modalVisible}
+        onRequestClose={() => {
+          Alert.alert('Modal has been closed.');
+          setModalVisible(!modalVisible);
+          hideModal();
+        }}
+      >
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Text style={styles.modalText}>Here are the instructions to add a new morphologie!</Text>  
+            <Text style={styles.modalText}>- Take a picture of your face</Text>
+            <Text style={styles.modalText}>- Put your measures after</Text>
+            <Pressable
+              style={[styles.button, styles.buttonClose]}
+              onPress={() => setModalVisible(!modalVisible)}>
+              <Text style={styles.textStyle}>Hide Instructions</Text>
+            </Pressable>
+          </View>
+        </View>
+      </Modal>
+      <Pressable
+        style={[styles.button, styles.buttonOpen]}
+        onPress={() => setModalVisible(true)}>
+        <Text style={styles.textStyle}>Show Instructions</Text>
+      </Pressable>
     </View>
   );
 }
@@ -14,7 +42,48 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-  }
+    marginTop: 22,
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 20,
+    padding: 35,
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+  button: {
+    borderRadius: 20,
+    padding: 10,
+    elevation: 2,
+  },
+  buttonOpen: {
+    backgroundColor: '#F194FF',
+  },
+  buttonClose: {
+    backgroundColor: '#2196F3',
+  },
+  textStyle: {
+    color: 'white',
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  modalText: {
+    marginBottom: 15,
+    textAlign: 'center',
+  },
 });
 
 export default ReconstructionMorphologieScreen;
